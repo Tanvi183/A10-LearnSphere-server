ab3047b63a8ba6ccbc29786392270181b6ff34af
tanvi183 <mneshat7@gmail.com>
2025-11-09 22:02:55 -0800

    application connect with mongodb database and sotre credintials in env file. To access user credintials install dotenv dependency.

--- index.js
+++ index.js
@@ -2,15 +2,41 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@simple-crud-server.7fhuvu7.mongodb.net/?appName=simple-crud-server`;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// Mongobd client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Server is running.....");
 });
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
